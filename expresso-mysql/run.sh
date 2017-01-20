f4dc0c87593e84c08c71fc39c4db14bc3f5bceeb
garret.meier <garret.meier@bunchball.com>
2017-01-19 20:12:28 -0600

    broke down compose files

--- expresso-mysql/run.sh
+++ expresso-mysql/run.sh
@@ -1,2 +1,2 @@
 eval $(docker-machine env)
-docker run -d -p 3306:3306 -e STARTUP_SQL="/scripts/create_tables.sql" ghmeier/expresso-mysql:latest
+docker run -d -p 3306:3306 -e STARTUP_SQL="/scripts/create_tables.sql" -e MYSQL_PASS="bloodlines" ghmeier/expresso-mysql:latest
